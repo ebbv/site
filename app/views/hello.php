3cec324fbd0b3e9f7e47e38436f75af0e4be1ada
Robert Doucette <rice8204@gmail.com>
2014-06-08 20:20:18 +0200

    Integrating the Foundation front-end framework

--- app/views/hello.php
+++ app/views/hello.php
@@ -5,8 +5,7 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/app.css">
         <script src="js/vendor/modernizr-2.8.2.min.js"></script>
     </head>
     <body>
@@ -18,8 +17,7 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+        <script src="js/app.js"></script>
 
     </body>
 </html>
