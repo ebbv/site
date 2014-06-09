f4bdfc894071690c86155da10c228cb7da9ceff7
Robert Doucette <rice8204@gmail.com>
2014-06-09 18:07:42 +0200

    Bringing in the rest of the pieces for the message player

--- app/routes.php
+++ app/routes.php
@@ -11,13 +11,84 @@
 |
 */
 
-Route::get('/', 'MessagesController@index');
+Route::get('connexion.html', array('before'=>'guest', function()
+{
+    if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] !== Request::root().'/connexion.html')
+    {
+        $goto = $_SERVER['HTTP_REFERER'];
+    }
+    else {
+        $goto = Input::get('goto');
+    }
+
+    return View::make('login')->with('goto', $goto);
+}));
+
+Route::post('connexion.html', function()
+{
+    $rules = array('username'=>'required', 'password'=>'required');
+    $v = Validator::make(Input::all(), $rules);
+    if ($v->passes())
+    {
+        if (Auth::attempt(array('email'=>Input::get('username'), 'password'=>Input::get('password'))))
+        {
+            $user = Member::find(Auth::user()->id);
+            $user->updated_by   = Auth::user()->id;
+            $user->last_login   = Auth::user()->current_login;
+            $user->current_login= date('Y-m-d H:i:s');
+            $user->save();
+            return Redirect::intended(Input::get('goto'));
+        }
+        Session::flash('login_error', 'Mauvaise combinaison, veuillez réessayer.');
+    }
+    return Redirect::to('connexion.html')->withInput(Input::except('password'))->withErrors($v);
+});
+
+Route::get('déconnexion.html', function()
+{
+    Auth::logout();
+    return Redirect::to('/');
+});
 
 
-Route::get('messages.html', array(
-    'as'    => 'messages.index',
-    'uses'  => 'MessagesController@index'
-));
+Route::group(array('before'=>'auth'), function()
+{
+    Route::get('messages/ajouter.html', array('as' => 'message.create', function()
+    {
+        $files = array();
+        foreach((File::glob('../tmp/*.mp3')) ? : array() as $file)
+        {
+            $files[]= trim($file, '\.\.\/tmp\.mp3');
+        }
+        return View::make('messages.create')->with('speakers', Member::has('speaker')->get())->withFiles($files);
+    }));
+
+    Route::post('messages.html', array('as' => 'message.store', function()
+    {
+        $filename = Str::random(15);
+        $message = new Message;
+        $message->member_id = Input::get('speaker');
+        $message->title     = Input::get('title');
+        $message->url       = $filename;
+        $message->date      = Input::get('message-date');
+        $message->created_by= Auth::user()->id;
+        $message->updated_by= Auth::user()->id;
+        if($message->save())
+        {
+            foreach(array('mp3', 'ogg', 'wav') as $ext)
+            {
+                File::move('../tmp/'.Input::get('message-file').'.'.$ext, public_path().'/audio/'.$filename.'.'.$ext);
+            }
+        }
+
+        return Redirect::route('message.create');
+    }));
+});
+
+Route::get('/', function() {
+    return View::make('messages.main')->with('messages', Message::with('speaker')->get());
+});
+
 
 View::creator(Config::get('app.theme'), function($view)
 {
--- app/views/messages/main.blade.php
+++ app/views/messages/main.blade.php
@@ -1,5 +1,52 @@
 @extends(Config::get('app.theme'))
 
 @section('content')
-                    <p>Les messages apparaitront ici.</p>
+                    <div class="row">
+                        <p class="small-12 columns">
+                            Ici nous vous proposons l'écoute des messages de notre église. Il vous
+                            suffit de cliquer sur le titre du message pour démarrer la lecture. Notre
+                            désir est d'édifier l'Eglise de Christ, et ce par un enseignement fidèle
+                            à Dieu et sa Parole. Si vous rencontrez des problèmes, n'hésitez pas à
+                            {{ link_to('contact.html', 'nous contacter.')."\n" }}
+                        </p>
+                    </div>
+                    <div class="row">
+                        <div class="small-12 columns" id="messages-table">
+@foreach($messages as $message)
+                            <div class="row">
+                                <div class="small-4 columns full-name">
+                                    <p>
+                                        <span class="last-name">{{ $message->speaker->last_name }}</span>,
+                                        {{ $message->speaker->first_name."\n" }}
+                                    </p>
+                                </div>
+                                <div class="small-4 columns message-date">
+                                    <p>{{ $message->date }}</p>
+                                </div>
+                                <div class="small-4 columns message-title">
+                                    <p>{{ $message->title }}</p>
+                                </div>
+                            </div>
+                            <div class="row player">
+                                <div class="small-1 medium-1 large-1 columns">
+                                    <span class="playtoggle"></span>
+                                </div>
+                                <div class="small-6 medium-7 large-8 columns">
+                                    <span class="gutter">
+                                        <span class="loading"></span>
+                                        <span class="handle ui-slider-handle"></span>
+                                    </span>
+                                </div>
+                                <div class="small-5 medium-4 large-3 columns text-center">
+                                    <span class="timeleft"></span>
+                                    <audio preload="metadata" buffered>
+                                        <source src="audio/{{ $message->url.'.ogg' }}" type="audio/ogg">
+                                        <source src="audio/{{ $message->url.'.mp3' }}" type="audio/mpeg">
+                                        <source src="audio/{{ $message->url.'.wav' }}" type="audio/x-wav">
+                                    </audio>
+                                </div>
+                            </div>
+@endforeach
+                        </div>
+                    </div>
 @stop
