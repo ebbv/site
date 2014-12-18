00201ea15ccdc1d846f8f21893682d974a43c8f9
Robert Doucette <rice8204@gmail.com>
2014-12-18 09:25:47 +0100

    Adding a missing comma

--- app/config/staging/app.php
+++ app/config/staging/app.php
@@ -3,6 +3,6 @@
 return array(
     'ga_key'        => 'XXXXXXXX-X',
     'debug'         => true,
-    'site_prefix'   => 'test - '
+    'site_prefix'   => 'test - ',
     'url'           => 'http://test.ebbv.fr',
 );
