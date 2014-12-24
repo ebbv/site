11a6204d11c67cd1ddf926c58bffc3af23b48938
Robert Doucette <rice8204@gmail.com>
2014-12-24 01:40:45 +0100

    Continuing the project restructuring

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,58 @@
+var del = require('del');
+
 var gulp = require('gulp');
+
+var pkg = require('./package.json');
 
 var plugins = require('gulp-load-plugins')();
 
-gulp.task('copy', [
-  'copy:jquery',
-  'copy:normalize'
-]);
+var runSequence = require('run-sequence');
 
-gulp.task('copy:normalize', function () {
+
+gulp.task('copy', function () {
+  return gulp.src('src/**', {dot:true})
+  .pipe(gulp.dest('public'));
+});
+
+gulp.task('normalize.css', function () {
   return gulp.src('node_modules/normalize.css/normalize.css')
   .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('copy:jquery', function () {
+gulp.task('jquery', function () {
   return gulp.src('node_modules/jquery/dist/jquery.min.js')
   .pipe(gulp.dest('public/js/vendor'));
 });
 
-gulp.task('css', function () {
+gulp.task('minify', function () {
   return gulp.src('public/css/*.css')
-  .pipe(plugins.minifyCss())
+  .pipe(plugins.minifyCss({keepSpecialComments:0}))
   .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('js', function () {
-  return gulp.src('public/js/*.js')
-  .pipe(plugins.uglify())
-  .pipe(gulp.dest('public/js'));
+gulp.task('prep', function (done) {
+  del([
+    'public/**',
+    '!audio'
+  ], done);
 });
 
-gulp.task('default', ['copy'], function () {
-  gulp.start('css', 'js');
+gulp.task('clean', function (done) {
+  del([
+    'public/css/*.css',
+    '!public/css/app.css'
+  ], done);
 });
+
+gulp.task('build', function (done) {
+  runSequence(
+    'prep',
+    'copy',
+    'normalize.css',
+    'jquery',
+    'minify',
+    'clean',
+  done)
+});
+
+gulp.task('default', ['build']);
