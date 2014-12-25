a73e6c9f4055b9f36826b7a868f17f6343c7af00
Robert Doucette <rice8204@gmail.com>
2014-12-25 11:46:10 +0100

    Switching to a local copy of the Google Analytics tracking script

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -45,7 +45,7 @@
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
             function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
             e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
+            e.src='js/vendor/analytics.js';
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-{{ Config::get('app.ga_key') }}','auto');ga('send','pageview');
         </script>
