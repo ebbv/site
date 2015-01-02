3fc6fc16d8c121f159890d21d5f3fe2980d5fdf1
Robert Doucette <rice8204@gmail.com>
2015-01-02 08:12:58 +0100

    Updating the footer text for 2015

--- src/app/views/layouts/footer.blade.php
+++ src/app/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
         <footer>
             <div>
-                <p id="copyright">&copy; Eglise Biblique Baptiste de Vernon {{ date('Y')}}</p>
+                <p id="copyright">&copy; Eglise Biblique Baptiste de Vernon 2014 - {{ date('Y') }}</p>
             </div>
         </footer>
