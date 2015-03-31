e10c420a49c61c05babc5e9efd3ac4ecc52f2d9a
Robert Doucette <rice8204@gmail.com>
2015-03-31 16:19:07 +0000

    Making sure the Cloud9 configs are being tracked

---
+++ src/app/config/c9/app.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'ga_key'        => 'XXXXXXXX-X',
+    'debug'         => true,
+    'site_prefix'   => 'dev - ',
+    'url'           => 'http://site-robbyrice-5.c9.io/',
+);
