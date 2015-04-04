94b1a6a60527aab7ceb7f03c26c4c3345b01aa26
Robert Doucette <rice8204@gmail.com>
2015-04-04 12:38:24 +0000

    Incrementing the url after rebuilding the dev environment

--- src/app/config/c9/app.php
+++ src/app/config/c9/app.php
@@ -4,5 +4,5 @@ return array(
     'ga_key'        => 'XXXXXXXX-X',
     'debug'         => true,
     'site_prefix'   => 'dev - ',
-    'url'           => 'http://site-robbyrice-5.c9.io/',
+    'url'           => 'http://site-robbyrice-6.c9.io/',
 );
