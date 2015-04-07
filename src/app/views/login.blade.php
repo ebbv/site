4ef539f15d27928fff4aedb4a4a2bf13bb0f0228
Robert Doucette <rice8204@gmail.com>
2015-04-07 12:44:01 +0000

    Configuring js validation on the login form

--- src/app/views/login.blade.php
+++ src/app/views/login.blade.php
@@ -2,21 +2,23 @@
 
 @section('content')
                 <div id="content" class="small-12 medium-4 medium-centered columns">
-                    {{ Form::open(array('url' => 'connexion.html', 'id' => 'login')) }}
+@if(Session::get('login_error'))
+                    <div>
+                        <small class="error">{{ Session::get('login_error') }}</small>
+                    </div>
+@endif
+                    {{ Form::open(array('url' => 'connexion.html', 'id' => 'login', 'data-abide')) }}
                         <div>
-                            <small <?php if(Session::get('login_error')) echo 'class="error"'; ?>>{{ Session::get('login_error') }}</small>
+                            <label>Nom d'utilisateur
+                                <input autofocus id="username" name="username" type="text" value="{{ Input::old('username') }}" required />
+                            </label>
+                            <small class="error">Obligatoire</small>
                         </div>
-                        <label>Nom d'utilisateur
-                            <input autofocus id="username" name="username" type="text" value="{{ Input::old('username') }}" />
-                        </label>
                         <div>
-                            <small <?php if($errors->has('username')) echo 'class="error"'; ?>>{{ $errors->first('username') }}</small>
-                        </div>
-                        <label>Mot de passe
-                            <input id="password" name="password" type="password" />
-                        </label>
-                        <div>
-                            <small <?php if($errors->has('password')) echo 'class="error"'; ?>>{{ $errors->first('password') }}</small>
+                            <label>Mot de passe
+                                <input id="password" name="password" type="password" required />
+                            </label>
+                            <small class="error">Obligatoire</small>
                         </div>
                         <input name="goto" type="hidden" value="{{ Input::old('goto', $goto) }}" />
                         <input type="submit" value="Se connecter" />
