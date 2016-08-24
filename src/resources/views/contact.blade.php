e827e6cacb00d8ecc373e2cc35863a7aa533f7c3
Robert Doucette <rice8204@gmail.com>
2016-08-24 12:14:24 +0200

    Adding service times to the contact page

--- src/resources/views/contact.blade.php
+++ src/resources/views/contact.blade.php
@@ -5,6 +5,11 @@
           <div class="row" id="contact">
             <div class="small-12 medium-7 columns">
               <h3>Nous rendre visite</h3>
+              <div class="meeting-times">
+                <p>Dimanche 9H45 - Formation biblique</p>
+                <p>Dimanche 10H30 - Culte d'adoration</p>
+                <p>Mardi 19H30 - Etude biblique et réunion de prière</p>
+              </div>
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d653.112762641644!2d1.4774316999999892!3d49.09707109999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x24d87fa6b1ae5cba!2sEglise+Biblique+Baptiste!5e0!3m2!1sen!2s!4v1428444318261" width="100%" height="300" frameborder="0" style="border:0"></iframe>
             </div>
             <div class="medium-5 columns">
