cdfa3de0926fadf1380f7d151ba18f3a5b2838df
Robert Doucette <rice8204@gmail.com>
2016-11-18 15:20:30 +0100

    Moving the Request type-hinting to the construct method in the Auth controller

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -12,14 +12,15 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-  public function __construct()
+  public function __construct(Request $r)
   {
+    $this->r = $r;
     $this->middleware('guest', ['only' => 'login']);
   }
 
-  public function login(Request $r)
+  public function login()
   {
-    $goto = $r->goto;
+    $goto = $this->r->goto;
 
     if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] !== url('connexion'))
     {
@@ -29,23 +30,23 @@ class AuthController extends Controller
     return view('login')->withGoto($goto);
   }
 
-  public function verify(Request $r)
+  public function verify()
   {
     $rules = ['username'=>'required', 'password'=>'required'];
-    $v = Validator::make($r->all(), $rules);
+    $v = Validator::make($this->r->all(), $rules);
     if ($v->passes())
     {
-      if (Auth::attempt(['username'=>$r->username, 'password'=>$r->password], true))
+      if (Auth::attempt(['username'=>$this->r->username, 'password'=>$this->r->password], true))
       {
         DB::table('logins')->insert([
           'member_id' => Auth::id(),
           'time'      => date('Y-m-d H:i:s')
         ]);
-        return redirect()->intended($r->goto);
+        return redirect()->intended($this->r->goto);
       }
-      $r->session()->flash('login_error', 'Mauvaise combinaison, veuillez réessayer.');
+      $this->r->session()->flash('login_error', 'Mauvaise combinaison, veuillez réessayer.');
     }
-    return redirect('connexion')->withInput($r->except('password'))->withErrors($v);
+    return redirect('connexion')->withInput($this->r->except('password'))->withErrors($v);
   }
 
   public function logout()
