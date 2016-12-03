4f44cc94ddb55de97ee39b1418191aad93be31f6
Robert Doucette <rice8204@gmail.com>
2016-12-04 00:11:30 +0100

    Continuing to adhere to psr-2

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -8,50 +8,53 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-  public function __construct(Request $r)
-  {
-    $this->r = $r;
-    $this->middleware('guest', ['only' => 'login']);
-  }
-
-  public function login()
-  {
-    $goto = $this->r->goto;
-
-    if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] !== url('connexion'))
+    public function __construct(Request $r)
     {
-      $goto = $_SERVER['HTTP_REFERER'];
+        $this->r = $r;
+        $this->middleware('guest', ['only' => 'login']);
     }
 
-    return view('login')->withGoto($goto);
-  }
-
-  public function verify()
-  {
-    $this->validate($this->r, [
-      'username' => 'required',
-      'password' => 'required'
-    ]);
-
-    if (Auth::attempt(['username'=>$this->r->username, 'password'=>$this->r->password], true))
+    public function login()
     {
-      DB::table('logins')->insert([
-        'member_id' => Auth::id(),
-        'time'      => date('Y-m-d H:i:s')
-      ]);
-      return redirect()->intended($this->r->goto);
-    }
-    $this->r->session()->flash('login_error', 'Mauvaise combinaison, veuillez réessayer.');
-    return back()->withInput($this->r->except('password'));
-  }
+        $goto = $this->r->goto;
 
-  public function logout()
-  {
-    DB::table('logouts')->insert([
-      'member_id' => Auth::id(),
-      'time'      => date('Y-m-d H:i:s')
-    ]);
-    Auth::logout();
-    return redirect('/');
-  }
+        if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] !== url('connexion')) {
+            $goto = $_SERVER['HTTP_REFERER'];
+        }
+
+        return view('login')->withGoto($goto);
+    }
+
+    public function verify()
+    {
+        $this->validate($this->r, [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt(['username'=>$this->r->username, 'password'=>$this->r->password], true)) {
+            DB::table('logins')->insert([
+                'member_id' => Auth::id(),
+                'time'      => date('Y-m-d H:i:s')
+            ]);
+
+            return redirect()->intended($this->r->goto);
+        }
+
+        $this->r->session()->flash('login_error', 'Mauvaise combinaison, veuillez réessayer.');
+
+        return back()->withInput($this->r->except('password'));
+    }
+
+    public function logout()
+    {
+        DB::table('logouts')->insert([
+            'member_id' => Auth::id(),
+            'time'      => date('Y-m-d H:i:s')
+        ]);
+
+        Auth::logout();
+
+        return redirect('/');
+    }
 }
--- src/app/Pagination.php
+++ src/app/Pagination.php
@@ -6,7 +6,6 @@ use Landish\Pagination\ZurbFoundation;
 
 class Pagination extends ZurbFoundation
 {
-
     protected $activePageWrapper = '<li class="current">%s</li>';
 
     protected $disabledPageWrapper = '<li class="disabled">%s</li>';
