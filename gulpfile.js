5db9c9e560ded7b41419a7447696621d785b8a47
Robert Doucette <rice8204@gmail.com>
2016-12-05 22:55:24 +0100

    Switching to using Laravel Elixir for builds tasks and reworking the project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,28 @@
-var del = require('del');
+var elixir = require('laravel-elixir');
 
-var gulp = require('gulp');
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
 
-var pkg = require('./bower.json');
+elixir.config.css.sass.pluginOptions.includePaths = './node_modules/foundation-sites/scss/';
 
-var plugins = require('gulp-load-plugins')();
-
-var runSequence = require('run-sequence');
-
-
-gulp.task('copy', function () {
-  return gulp.src(['src/**', '!src/bootstrap/cache', '!src/resources/views/layouts/master.blade.php'], {dot:true})
-  .pipe(gulp.dest('./'));
+elixir(function(mix) {
+    mix.sass([
+        'app.scss'
+    ])
+    .scripts('app.js', 'public/js/app.js')
+    .copy([
+        'node_modules/foundation-sites/dist/foundation.min.js',
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/jquery-ui/jquery-ui.min.js'
+    ], 'public/js/vendor')
+    .copy('bower_components/jquery-ui/themes/ui-lightness/images', 'public/css/images')
+    .copy('resources/assets/imgs', 'public/imgs');
 });
-
-gulp.task('css', function () {
-  return gulp.src([
-    'node_modules/foundation-sites/dist/foundation.min.css',
-    'bower_components/jquery-ui/themes/ui-lightness/jquery-ui.min.css',
-    'bower_components/jquery-ui/themes/ui-lightness/**',
-    'node_modules/normalize.css/normalize.css'
-  ])
-  .pipe(gulp.dest('public/css'));
-});
-
-gulp.task('js', function () {
-  return gulp.src([
-    'node_modules/foundation-sites/dist/foundation.js',
-    'bower_components/jquery/dist/jquery.js',
-    'bower_components/jquery-ui/jquery-ui.js'
-  ])
-  .pipe(plugins.rename(function (path) {
-    path.basename += ".min";
-  }))
-  .pipe(plugins.uglify())
-  .pipe(gulp.dest('public/js/vendor'));
-});
-
-gulp.task('template', function () {
-  return gulp.src('src/resources/views/layouts/master.blade.php')
-  .pipe(plugins.replace(/{{JQUERY_VERSION}}/g, pkg.dependencies.jquery))
-  .pipe(gulp.dest('resources/views/layouts'));
-});
-
-gulp.task('minify', function () {
-  return gulp.src('public/css/*.css')
-  .pipe(plugins.cleanCss({keepSpecialComments:0}))
-  .pipe(gulp.dest('public/css'));
-});
-
-gulp.task('imgs', function () {
-  return gulp.src('public/img/**')
-  .pipe(plugins.imagemin({optimizationLevel:3, progressive:true, interlaced:true}))
-  .pipe(gulp.dest('public/img'));
-});
-
-gulp.task('prep', (done) => {
-  del([
-    'app',
-    'bootstrap/*',
-    '!bootstrap/cache',
-    'config',
-    'database',
-    'public/*',
-    '!public/audio',
-    'resources',
-    'tests'
-  ]).then( () => {
-      done();
-  });
-});
-
-gulp.task('clean', (done) => {
-  del([
-    'public/css/*.css',
-    '!public/css/app.css'
-  ]).then( () => {
-      done();
-  });
-});
-
-gulp.task('build', function (done) {
-  runSequence(
-    'prep',
-    'copy',
-    'css',
-    'js',
-    'template',
-    'minify',
-    'imgs',
-    'clean',
-  done)
-});
-
-gulp.task('default', ['build']);
