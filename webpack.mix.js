3c8c6049cac5a873fd9f3141b09459b6de3b73a1
Robert Doucette <rice8204@gmail.com>
2017-08-27 23:28:58 +0200

    Updating Material Design Components to v0.19 and reworking Mix config slightly

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,18 +3,13 @@ const { mix } = require('laravel-mix');
 mix.options({})
   .webpackConfig ({
     module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules(?!\/@material)|bower_components/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: mix.config.babel()
-            }
-          ]
-        }
-      ]
+      rules: [{
+        test: /\.jsx?$/,
+        use: [{
+          loader: 'babel-loader',
+          options: mix.config.babel()
+        }]
+      }]
     }
   })
   .sass('resources/assets/sass/app.scss', 'public/css', {
