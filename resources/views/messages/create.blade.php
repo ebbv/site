8a72f786eadbdd6f9b5d2b8cbb3818eac4e41def
Robert Doucette <rice8204@gmail.com>
2018-09-12 15:23:11 +0200

    Updates HTML structure and CSS of <select> elements

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -24,14 +24,15 @@
             <div class="mdc-layout-grid__cell
                         mdc-layout-grid__cell--span-3-tablet
                         mdc-layout-grid__cell--span-4-desktop">
-              <div class="mdc-select">
+              <div class="mdc-select mdc-select--box">
                 <select class="mdc-select__native-control" id="speaker" name="user_id">
-                  <!-- <option></option> -->
+                  <option disabled selected value=""></option>
 @foreach ($speakers as $s)
                   <option value="{{ $s->id }}">{{ $s->last_name }}, {{ $s->first_name }}</option>
 @endforeach
                 </select>
-                <div class="mdc-select__bottom-line"></div>
+                <label class="mdc-floating-label" for="speaker">Prédicateur</label>
+                <div class="mdc-line-ripple"></div>
               </div>
             </div>
             <div class="mdc-layout-grid__cell
@@ -51,14 +52,15 @@
             <div class="mdc-layout-grid__cell
                         mdc-layout-grid__cell--span-2-tablet
                         mdc-layout-grid__cell--span-3-desktop">
-              <div class="mdc-select">
+              <div class="mdc-select mdc-select--box">
                 <select class="mdc-select__native-control" id="message-file" name="file">
-                  <option default selected>Choisir un fichier...</option>
+                  <option disabled selected value=""></option>
 @foreach ($files as $f)
                   <option value="{{ $f }}">{{ $f }}</option>
 @endforeach
                 </select>
-                <div class="mdc-select__bottom-line"></div>
+                <label class="mdc-floating-label" for="message-file">Choisir un fichier</label>
+                <div class="mdc-line-ripple"></div>
               </div>
             </div>
             <div class="mdc-layout-grid__cell
