cca33b771fface342d30167459cd344b0e9ef70d
Robert Doucette <rice8204@gmail.com>
2018-09-13 02:57:28 +0200

    Updates the Laravel Framework to v5.7

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -67,7 +68,7 @@ class RegisterController extends Controller
             'first_name'=> $data['first_name'],
             'last_name' => $data['last_name'],
             'username'  => $data['username'],
-            'password'  => bcrypt($data['password']),
+            'password'  => Hash::make($data['password']),
         ]);
     }
 }
