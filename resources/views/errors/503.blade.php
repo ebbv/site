66f03992ee0995a5639bf776f1159c79a64479f6
Robert Doucette <rice8204@gmail.com>
2018-10-12 13:50:57 +0200

    Creates a custom page view for HTTP 503 errors (also known as maintenance mode)

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,8 @@
+@extends(config('app.theme'))
+
+@section('content')
+          <p class="mdc-typography--headline2 error-503">
+            Nous travaillons actuellement sur notre site,
+            merci de patienter quelques instants.
+          </p>
+@endsection
