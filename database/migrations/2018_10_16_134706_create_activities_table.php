096fb17ac0d6f356cf8784e3d6b5c2985d6c7197
Robert Doucette <rice8204@gmail.com>
2018-10-20 11:59:32 +0200

    Adds the migration for creating the activities table

---
+++ database/migrations/2018_10_16_134706_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->morphs('subject');
+            $table->string('type');
+            $table->text('before')->nullable()->default(null);
+            $table->text('after');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
