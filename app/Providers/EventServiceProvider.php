1c826a32d317265ab9d46de347f8f2be9d410c38
Robert Doucette <rice8204@gmail.com>
2019-05-22 22:24:14 +0200

    Updates the Laravel Framework to v5.8

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
@@ -14,7 +15,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        \Illuminate\Auth\Events\Registered::class => [
+        Registered::class => [
             SendEmailVerificationNotification::class,
         ],
         \Illuminate\Auth\Events\Login::class => [
