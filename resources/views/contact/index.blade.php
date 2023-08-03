b0fdbf08284c3e2f6bf76c11fb2c177f0b0ea7e2
Robert Doucette <rice8204@gmail.com>
2023-08-03 23:48:48 +0200

    Update Tuesday evening prayer meeting time

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -7,7 +7,7 @@
               <h3>Nous rendre visite</h3>
               <p>Dimanche 9H45 - Formation biblique</p>
               <p>Dimanche 10H30 - Culte d'adoration</p>
-              <p>Mardi 19H30 - Etude biblique et réunion de prière</p>
+              <p>Mardi 19H00 - Etude biblique et réunion de prière</p>
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d653.112762641644!2d1.4774316999999892!3d49.09707109999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x24d87fa6b1ae5cba!2sEglise+Biblique+Baptiste!5e0!3m2!1sen!2s!4v1428444318261" width="100%" height="300" frameborder="0" style="border:0"></iframe>
             </div>
             <div class="mdc-layout-grid__cell
