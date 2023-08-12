68320544110b9f180cec433a8705da7b66f525ac
Robert Doucette <rice8204@gmail.com>
2023-08-12 11:06:17 +0200

    Update the styling for the bulletin links

--- resources/views/bulletin/index.blade.php
+++ resources/views/bulletin/index.blade.php
@@ -1,7 +1,13 @@
 @extends(config('app.theme'))
 
 @section('content')
-  <a href="bulletin/2023/juillet">juillet</a>
-  <a href="bulletin">mois en cours</a>
+  <a class="mdc-button mdc-button--raised" href="bulletin/2023/juillet">
+    <div class="mdc-button__ripple"></div>
+    <span class="mdc-button__label">juillet</span>
+  </a>
+  <a class="mdc-button mdc-button--raised" href="bulletin">
+    <div class="mdc-button__ripple"></div>
+    <span class="mdc-button__label">mois en cours</span>
+  </a>
   <embed id="bulletin-iframe" type="application/pdf" src="{{ $url }}?generate"></embed>
 @endsection
