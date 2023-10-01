a24d308eda0cf8582337afa3f843bd24e8ba0154
Robert Doucette <rice8204@gmail.com>
2023-10-01 06:15:51 +0200

    Update Bible reading for October 2023

--- resources/views/bible-reading/index.blade.php
+++ resources/views/bible-reading/index.blade.php
@@ -2,12 +2,12 @@
 
 @php
 $passages = [
-  'Exode 24-25', 'Exode 26-27', 'Exode 28', 'Exode 29', 'Exode 30-31', 'Exode 32-33',
-  'Exode 34', 'Exode 35-36', 'Exode 37-38', 'Exode 39', 'Exode 40', 'Lévitique 1',
-  'Lévitique 2-3', 'Lévitique 4', 'Lévitique 5-6', 'Lévitique 7', 'Lévitique 8-9',
-  'Lévitique 10', 'Lévitique 11-12', 'Lévitique 13', 'Lévitique 14', 'Lévitique 15',
-  'Lévitique 16', 'Lévitique 17-18', 'Lévitique 19', 'Lévitique 20-21', 'Lévitique 22',
-  'Lévitique 23-24', 'Lévitique 25', 'Lévitique 26'
+  'Lévitique 27', 'Nombres 1', 'Nombres 2', 'Nombres 3', 'Nombres 4', 'Nombres 5-6',
+  'Nombres 7', 'Nombres 8-9', 'Nombres 10', 'Nombres 11-12', 'Nombres 13-14',
+  'Nombres 15', 'Nombres 16-17', 'Nombres 18', 'Nombres 19-20', 'Nombres 21',
+  'Nombres 22', 'Nombres 23-24', 'Nombres 25-26', 'Nombres 27-28', 'Nombres 29-30',
+  'Nombres 31', 'Nombres 32', 'Nombres 33', 'Nombres 34', 'Nombres 35-36',
+  'Deutéronome 1', 'Deutéronome 2', 'Deutéronome 3', 'Deutéronome 4', 'Deutéronome 5'
 ]
 @endphp
 
