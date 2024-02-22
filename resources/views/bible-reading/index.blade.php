bb134b2c8c11ae5ccdee2f0000475f29ef441c4e
Robert Doucette <rice8204@gmail.com>
2024-02-22 19:12:02 +0100

    Update Bible reading for February 2024

--- resources/views/bible-reading/index.blade.php
+++ resources/views/bible-reading/index.blade.php
@@ -2,11 +2,10 @@
 
 @php
 $passages = [
-  '1 Samuel 1', '1 Samuel 2', '1 Samuel 3-4', '1 Samuel 5-6', '1 Samuel 7-8', '1 Samuel 9', '1 Samuel 10-11',
-  '1 Samuel 12-13', '1 Samuel 14', '1 Samuel 15', '1 Samuel 16', '1 Samuel 17', '1 Samuel 18-19',
-  '1 Samuel 20', '1 Samuel 21-22', '1 Samuel 23-24', '1 Samuel 25', '1 Samuel 26-27', '1 Samuel 28-29',
-  '1 Samuel 30-31', '2 Samuel 1', '2 Samuel 2', '2 Samuel 3-4', '2 Samuel 5-6', '2 Samuel 7-8',
-  '2 Samuel 9-10', '2 Samuel 11', '2 Samuel 12', '2 Samuel 13', '2 Samuel 14', '2 Samuel 15'
+  '2 Samuel 16-17', '2 Samuel 18', '2 Samuel 19', '2 Samuel 20', '2 Samuel 21', '2 Samuel 22', '2 Samuel 23',
+  '2 Samuel 24', '1 Rois 1', '1 Rois 2', '1 Rois 3-4', '1 Rois 5-6', '1 Rois 7', '1 Rois 8', '1 Rois 9-10',
+  '1 Rois 11', '1 Rois 12-13', '1 Rois 14', '1 Rois 15', '1 Rois 16-17', '1 Rois 18', '1 Rois 19-20', '1 Rois 21',
+  '1 Rois 22', '2 Rois 1-2', '2 Rois 3-4', '2 Rois 5-6', '2 Rois 7', '2 Rois 8'
 ]
 @endphp
 
