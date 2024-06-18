02dca4aeb363181ceea4c6ba8250e2b7a1b3e462
Robert Doucette <rice8204@gmail.com>
2024-06-18 15:46:58 +0200

    Update Bible reading for June 2024

--- resources/views/bible-reading/index.blade.php
+++ resources/views/bible-reading/index.blade.php
@@ -1,18 +1,15 @@
 @extends(config('app.theme'))
 
 @php
-$passages = [
-  '2 Chroniques 1-2', '2 Chroniques 3-4', '2 Chroniques 5-6', '2 Chroniques 7-8', '2 Chroniques 9-10', '2 Chroniques 11-12',
-  '2 Chroniques 13-14', '2 Chroniques 15-16', '2 Chroniques 17-18', '2 Chroniques 19-20', '2 Chroniques 21-22',
-  '2 Chroniques 23-24', '2 Chroniques 25', '2 Chroniques 26-27', '2 Chroniques 28', '2 Chroniques 29', '2 Chroniques 30-31',
-  '2 Chroniques 32-33', '2 Chroniques 34', '2 Chroniques 35-36', 'Esdras 1-2', 'Esdras 3-4', 'Esdras 5-6', 'Esdras 7-8',
-  'Esdras 9-10', 'Néhémie 1-2', 'Néhémie 3', 'Néhémie 4-5', 'Néhémie 6-7', 'Néhémie 8'
-]
+$passages = 'Psaumes 12-16, Psaumes 17-18, Psaumes 19-21, Psaumes 22-24, Psaumes 25-28, Psaumes 29-30, Psaumes 31-32,
+  Psaumes 33-34, Psaumes 35-36, Psaumes 37-38, Psaumes 39-41, Psaumes 42-44, Psaumes 45-48, Psaumes 49-50, Psaumes 51-54,
+  Psaumes 55-58, Psaumes 59-61, Psaumes 62-66, Psaumes 67-68, Psaumes 69-70, Psaumes 71-72, Psaumes 73-75, Psaumes 76-77,
+  Psaumes 78-79, Psaumes 80-83, Psaumes 84-87, Psaumes 88-89, Psaumes 90-93, Psaumes 94-96, Psaumes 97-101';
 @endphp
 
 @section('content')
   <ol id="bible-reading">
-@foreach ($passages as $passage)
+@foreach (explode(',', $passages) as $passage)
     <li>
       <span>{{ $passage }}</span>
       <div>o</div>
