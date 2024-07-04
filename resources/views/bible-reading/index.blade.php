ae6d900f3f78b8b97a3aae050d49ab5d0721b9c2
Robert Doucette <rice8204@gmail.com>
2024-07-04 13:19:44 +0200

    Update Bible reading for July 2024

--- resources/views/bible-reading/index.blade.php
+++ resources/views/bible-reading/index.blade.php
@@ -1,10 +1,11 @@
 @extends(config('app.theme'))
 
 @php
-$passages = 'Psaumes 12-16, Psaumes 17-18, Psaumes 19-21, Psaumes 22-24, Psaumes 25-28, Psaumes 29-30, Psaumes 31-32,
-  Psaumes 33-34, Psaumes 35-36, Psaumes 37-38, Psaumes 39-41, Psaumes 42-44, Psaumes 45-48, Psaumes 49-50, Psaumes 51-54,
-  Psaumes 55-58, Psaumes 59-61, Psaumes 62-66, Psaumes 67-68, Psaumes 69-70, Psaumes 71-72, Psaumes 73-75, Psaumes 76-77,
-  Psaumes 78-79, Psaumes 80-83, Psaumes 84-87, Psaumes 88-89, Psaumes 90-93, Psaumes 94-96, Psaumes 97-101';
+$passages = 'psaumes 102-103, psaumes 104-105, psaume 106, psaumes 107-108, psaumes 109-112, psaumes 113-116, psaumes 117-118,
+  psaume 119.1-32, psaume 119.33-64, psaume 119.65-96, psaume 119.97-128, psaume 119.129-160, psaume 119.161-176, psaumes 120-122,
+  psaumes 123-126, psaumes 127-131, psaumes 132-135, psaumes 136-138, psaumes 139-141, psaumes 142-144, psaumes 145-146,
+  psaumes 147-148, psaumes 149-150, proverbes 1-2, proverbes 3-4, proverbes 5-6, proverbes 7-8, proverbes 9-10, proverbes 11-12,
+  proverbes 13-14, proverbes 15';
 @endphp
 
 @section('content')
