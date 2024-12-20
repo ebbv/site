a19df54cb395c23cbd8107671346badc51b38208
Robert Doucette <rice8204@gmail.com>
2024-12-20 14:42:34 +0100

    Update Bible reading page for December 2024

--- resources/views/bible-reading/index.blade.php
+++ resources/views/bible-reading/index.blade.php
@@ -1,10 +1,10 @@
 @extends(config('app.theme'))
 
 @php
-$passages = 'jeremie 10, jeremie 11-12, jeremie 13, jeremie 14-15, jeremie 16, jeremie 17, jeremie 18-19,
-  jeremie 20-21, jeremie 22, eremie 23, jeremie 24-25, jeremie 26, jeremie 27-28, jeremie 29, jeremie 30-31,
-  jeremie 32, jeremie 33, jeremie 34, jeremie 35, jeremie 36, jeremie 37, jeremie 38, jeremie 39-40,
-  jeremie 41-42, jeremie 43-44, jeremie 45-46, jeremie 47-48, jeremie 49, jeremie 50, jeremie 51, jeremie 52'
+$passages = 'ezechiel 43-44, ezechiel 45-46, ezechiel 47, ezechiel 48, daniel 1-2, daniel 3, daniel 4,
+            daniel 5-6, daniel 7-8, daniel 9-10, daniel 11-12, osee 1-2, osee 3-5, osee 6-8, osee 9-11,
+            osee 12-14, joel 1-2, joel 3, amos 1-2, amos 3-4, amos 5-6, amos 7-9, abdias 1, jonas 1-2,
+            jonas 3-4, michee 1-2, michee 3-5, michee 6-7, nahum 1-3, habakuk 1-3, sophonie 1-2'
 @endphp
 
 @section('content')
