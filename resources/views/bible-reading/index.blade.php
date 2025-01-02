a45531b2d78fd1a1d1c61a8be2d072eda4b4d1ad
Robert Doucette <rice8204@gmail.com>
2025-01-02 15:26:50 +0100

    Update Bible reading page pour January 2025

--- resources/views/bible-reading/index.blade.php
+++ resources/views/bible-reading/index.blade.php
@@ -1,10 +1,11 @@
 @extends(config('app.theme'))
 
 @php
-$passages = 'ezechiel 43-44, ezechiel 45-46, ezechiel 47, ezechiel 48, daniel 1-2, daniel 3, daniel 4,
-            daniel 5-6, daniel 7-8, daniel 9-10, daniel 11-12, osee 1-2, osee 3-5, osee 6-8, osee 9-11,
-            osee 12-14, joel 1-2, joel 3, amos 1-2, amos 3-4, amos 5-6, amos 7-9, abdias 1, jonas 1-2,
-            jonas 3-4, michee 1-2, michee 3-5, michee 6-7, nahum 1-3, habakuk 1-3, sophonie 1-2'
+$passages = 'sophonie 3, aggee 1-2, zacharie 1-3, zacharie 4-7, zacharie 8-9, zacharie 10-12, zacharie 13-14,
+            malachie 1-2, malachie 3-4, matthieu 1-2, matthieu 3-4, matthieu 5, matthieu 6-7, matthieu 8,
+            matthieu 9, matthieu 10, matthieu 11, matthieu 12, matthieu 13, matthieu 14-15, matthieu 16-17,
+            matthieu 18, matthieu 19-20, matthieu 21, matthieu 22, matthieu 23, matthieu 24, matthieu 25,
+            matthieu 26, matthieu 27, matthieu 28'
 @endphp
 
 @section('content')
