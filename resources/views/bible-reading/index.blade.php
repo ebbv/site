fffd5d8b874f848a58be38c1b9b2a6bf9e6ccf51
Robert Doucette <rice8204@gmail.com>
2026-05-02 20:49:53 +0200

    Update Bible reading page for May 2026

--- resources/views/bible-reading/index.blade.php
+++ resources/views/bible-reading/index.blade.php
@@ -1,10 +1,9 @@
 @extends(config('app.theme'))
 
 @php
-$passages = '2 chroniques 1-2, 2 chroniques 3-4, 2 chroniques 5-6, 2 chroniques 7-8, 2 chroniques 9-10, 2 chroniques 11-12,
-             2 chroniques 13-14, 2 chroniques 15-16, 2 chroniques 17-18, 2 chroniques 19-20, 2 chroniques 21-22, 2 chroniques 23-24,
-             2 chroniques 25, 2 chroniques 26-27, 2 chroniques 28, 2 chroniques 29, 2 chroniques 30-31, 2 chroniques 32-33, 2 chroniques 34,
-             2 chroniques 35-36, esdras 1-2, esdras 3-4, esdras 5-6, esdras 7-8, esdras 9-10, nehemie 1-2, nehemie 3, nehemie 4-5, nehemie 6-7, nehemie 8';
+$passages = 'nehemie 9, nehemie 10-11, nehemie 12-13, esther 1-2, esther 3-4, esther 5-7, esther 8-10, job 1-2, job 3-4, job 5-6,
+             job 7-8, job 9-10, job 11-13, job 14-15, job 16-18, job 19-20, job 21, job 22-23, job 24-26, job 27-28, job 29-30,
+             job 31-32, job 33, job 34-35, job 36--37, job 38, job 39-40, job 41-42, psaumes 1-5, psaumes 6-8, psaumes 9-11';
 @endphp
 
 @section('content')
